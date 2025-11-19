979ee93888e170b42d458557b4b19128e4d280e1
Rachel Balta <rachelbalta@Rachels-MacBook-Air.local>
2025-11-19 14:32:24 -0800

    page and code set up

---
+++ turbo_chat/app/controllers/messages_controller.rb
@@ -0,0 +1,26 @@
+class  MessagesController < ApplicationController
+  before_action :set_room
+
+  def new
+    @message = @room.messages.new
+  end
+
+  def create
+    @messages = @room.messages.create!(message_params)
+
+    respond_to do |format|
+        format.html { redirect_to @room }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_room
+      @room = Room.find(params.require(:room_id))
+    end
+
+    # Only allow a list of trusted parameters through.
+    def room_params
+      params.expect(room: [ :name ])
+    end
+end
